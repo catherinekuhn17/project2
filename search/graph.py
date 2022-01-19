f9974088856945bb8c560fbab8fc09c007802fee
Catherine Kuhn <31172559+catherinekuhn17@users.noreply.github.com>
2022-01-19 00:07:17 -0800

    edited graph.py

--- search/graph.py
+++ search/graph.py
@@ -36,15 +36,16 @@ class Graph:
         
         Parameters
         ----------
-        self: network to traverse through
-        start: starting node
-        end: ending node
+        self : network to traverse through
+        start : starting node
+        end : ending node
 
         Returns
         -------
-        shortest_path : shortest path from start to end (if it exists). If it does not exist, None is resturned.
+        shortest_path : shortest path from start node to end node (if it exists) or the end of network. If there is no connection 
+            between the start and end node, None is returned.
         """
-        queue = []  #to know which node to visit next
+        queue = []  # to know which node to visit next in current layer
         visited = []  # will fill with visited nodes
         backtrace = {
         }  # creating a dict that we can backtrace from. format is {child : parent}
@@ -53,22 +54,22 @@ class Graph:
         visited.append(start)  # mark start as visited
 
         while queue:
-            current_node = queue.pop(0)  # keep track of current node
+            current_node = queue.pop(0) # keep track of current node (pulled from queue)
             path = [current_node] # set current node as end of path
             if current_node == end:  
                 break # break out of cycle if we reach the end
             else:
                 for nbr in self.graph[current_node]:  # otherwise, loop through neighbors
-                    if nbr not in visited:
+                    if nbr not in visited: # ensure neighbor hasn't been visited
                         queue.append(nbr)
                         visited.append(nbr)
-                        backtrace[nbr] = current_node  # keep track of this neihgbors parent for pathfinding
+                        backtrace[nbr] = current_node  # keep track of this neihgbor's parent for pathfinding
 
-        if end != None and end not in backtrace.keys():  # condition that there is and end node. but no path
+        if end != None and end not in backtrace.keys():  # condition that there is an end node, but no path to it
             return None
         
         # otherwise, we will backtrace through the parent:child dictionary we created, starting with the
-        # end node, or the end of the path (if no end node)
+        # end node, or the end of the path (if no end node provided)
 
         while path[-1] != start:  # loop through until we reach the start node
             child_node = path[-1] # last in path
